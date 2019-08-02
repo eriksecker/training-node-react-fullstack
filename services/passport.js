670b98fc0196573e9e68508ee3953174a9d41f3e
Erik <erik.secker@cognitivewave.com>
2019-08-02 11:34:23 -0400

    on auth callback, look for an existing user with the returned google id. if no existing user, create a new one

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
 	new GoogleStrategy({
@@ -9,8 +12,14 @@ passport.use(
 		clientSecret: keys.googleClientSecret,
 		callbackURL: '/auth/google/callback'
 	}, (accessToken, refreshToken, profile, done) => {
-		console.log('access token', accessToken);
-		console.log('refresh token', refreshToken);
-		console.log('profile', profile);
+		User.findOne({googleId: profile.id})
+			.then((existingUser) => {
+				if ( ! existingUser ) {
+					// create new user
+					new User({
+						googleId: profile.id,
+					}).save();
+				}
+			});
 	})
 );
