e9c1870b50425d48d6e600850c66e72d29b5d550
Erik <erik.secker@cognitivewave.com>
2019-08-06 21:19:19 -0400

    dev and prod env variables config

---
+++ config/prod.js
@@ -0,0 +1,13 @@
+const {
+	googleClientID,
+	googleClientSecret,
+	mongoURI,
+	cookieKey,
+} = process.env;
+
+modules.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY,
+}
